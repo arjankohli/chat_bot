5707ab7df9710642d2d15092e7e31c25eeaaf529
Arjan Kohli <arjankohli5812@gmail.com>
2015-10-26 13:43:21 -0400

    add generator file

---
+++ databaseGenerator.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <sqlite3.h>
+#include <fstream>
+using namespace std;
+
+void databaseGenerator();
+
+int main()
+{
+	databaseGenerator();
+	return 0;
+}
+
+void databaseGenerator()
+{
+        //Connect to the database, limited to old style c code by the sqlite
+        sqlite3 *db;
+        char *zErrMsg = 0;
+        int rc;
+
+        rc = sqlite3_open("vocabulary.db", &db);
+        if(rc)
+        {
+                cout << "Cant open database:" << sqlite3_errmsg(db);
+        }
+        else
+        {
+                cout << "Opened database successfully" << endl;
+		cout << "Writing files into the database!" << endl;
+
+		ifstream inputFile;
+		inputFile.open("input.txt");
+
+                //nouns, pronouns, verbs, adjectives, adverbs, prepositions$
+		
+
+        }
+        //Close the connection now that we are done with it.
+        sqlite3_close(db);
+
+}
