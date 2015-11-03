6d420bd77617e0f06b460f3ffa0e059a9689ef1a
arjankohli5812 <arjankohli5812@gmail.com>
2015-11-02 23:48:02 -0500

    Redesign of Functions and Creation of Sql Statements

--- main.cpp
+++ main.cpp
@@ -2,21 +2,13 @@
 
 Cbot is an *intelligent* chatting bot. Cbot is designed to make conversation based on sentence structure utilizing a knowledge database.
 
-First an input string is grabbed, formatted, broken into word segments and encased in sentence containers.
-The sentences are then logically interpreted by  first matching the sentences to common sentence structures in English,
-and then applying the words in context to develop a sentence meaning.
-Then a response is formulated based on the sentence interpretation
-
-Remember KISS
-
-For development in codeblocks use:
-#include <C:\Users\Arjan\codeblocks\sqlite3\sqlite3.h>
 */
 
 #include <iostream>
-#include <string>
-#include <vector>
 #include <fstream>
+#include <string>
+#include <cstring>
+#include <vector>
 #include <C:\Users\Arjan\codeblocks\sqlite3\sqlite3.h>
 
 using namespace std;
@@ -24,6 +16,9 @@ using namespace std;
 string getInput();
 vector<string> inputFormatter(string);
 void inputParser(vector<string>);
+static int callback(void *data, int argc, char **argv, char **azColName);
+void verifySql(int rc, char *zErrMsg);
+void generateDatabase();
 
 int main()
 {
@@ -38,7 +33,7 @@ int main()
 		//Get the user input, and format the input into a vector dividing words and punctuation
 		userMessage = inputFormatter(getInput());
 
-		//Function to parse input here
+		//Parse the input by determining the sentence type utilizing a database of vocabulary
 		inputParser(userMessage);
 
 		//Function to structure a response here
@@ -47,7 +42,12 @@ int main()
 		for(unsigned i = 0; i < userMessage.size(); i++)
 		{
 			cout << userMessage[i] << ' ';
+			if(userMessage[i] == "GENDATA")
+            {
+                generateDatabase();
+            }
 		}
+
 	}while(!userLeaving);
 
 	return 0;
@@ -62,6 +62,7 @@ string getInput()
 	return inputString;
 }
 
+//Format the input into a vector
 vector<string> inputFormatter(string userInputStr)
 {
 	vector<string> formattedInput(1);
@@ -92,19 +93,99 @@ void inputParser(vector<string> input)
 {
 	//Connect to the database, limited to old style c code by the sqlite header file
 	sqlite3 *db;
-	char *zErrMsg = 0;
-	int rc;
 
-	rc = sqlite3_open("vocabulary.db", &db);
-	if(rc)
+	//sqlite_open returns true if it fails...
+	if(sqlite3_open("vocabulary.db", &db))
 	{
-		cout << "Cant open database:" << sqlite3_errmsg(db);
+		cout << "Cant open database" << endl;
 	}
 	else
 	{
 		cout << "Opened database successfully" << endl;
 
+
 	}
 	//Close the connection now that we are done with it.
 	sqlite3_close(db);
+}
+
+static int callback(void *data, int argc, char **argv, char **azColName)
+{
+    int i;
+    fprintf(stderr, "%s: ", (const char*)data);
+    for(i=0; i<argc; i++)
+    {
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    }
+    printf("\n");
+
+    return 0;
+}
+
+void verifySql(int rc, char *zErrMsg)
+{
+    if( rc != SQLITE_OK )
+    {
+        cout << "SQL error: " << zErrMsg << endl;
+        sqlite3_free(zErrMsg);
+    }
+    else
+    {
+        cout << "Operation done successfully" << endl;
+    }
+}
+
+void generateDatabase()
+{
+    //Variables needed to make sql_lite statements
+    //*limited to c-strings by the sqlite header files
+    sqlite3 *db;
+    char *sql;
+    char *zErrMsg = 0;
+    int rc;
+    const char* data = "Callback function called";
+
+    ifstream inputFile;
+    inputFile.open("words.txt");
+    string word, type, insertStatement;
+
+    //Connect to the database
+    if(sqlite3_open("vocabulary.db", &db))
+    {
+        cout << "Cant open database!" << endl;
+    }
+    else if(!inputFile)
+    {
+        cout << "Input File not found!" << endl;
+    }
+    else
+    {
+        cout << "Opened database successfully" << endl;
+        cout << "Writing files into the database!" << endl;
+
+        //Create table if it does not exist.
+        sql = "CREATE TABLE IF NOT EXISTS Grammar(Word TEXT PRIMARY KEY    NOT NULL,Type TEXT    NOT NULL)";
+        rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
+        verifySql(rc, zErrMsg);
+
+        //proper nouns, nouns, pronouns, verbs, adjectives, adverbs, prepositions, articles
+        while(inputFile >> word >> type)
+        {
+            //Check for if the word + type exists, and if not create an entry for that word + type
+            insertStatement = "INSERT  INTO    Grammar VALUES ('" + word + "', '" + type + "')";
+
+            //Create cstring version of sql statement so it can pass into the sql arguement
+            int length = insertStatement.length() + 1;
+            char charSQl[length];
+            strcpy(charSQl, insertStatement.c_str());
+            sql = charSQl;
+
+            //Execute sql statement
+            rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
+            verifySql(rc, zErrMsg);
+        }
+
+        //Close the connection now that we are done with it.
+        sqlite3_close(db);
+    }
 }
